3aedfbd3d48196d1e585d47be061af0894bfa80e
ezequiel reyes <110490540+EzeReyes@users.noreply.github.com>
2024-05-27 14:00:50 -0300

    dandole forma a las demas paginas

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const art = document.querySelector("#art")
+const productos = document.querySelector(".productos")
 
 const options = {method: 'GET', headers: {accept: 'application/json'}};
 
@@ -12,7 +13,6 @@ const pedirDatos = () => {
                 `<div class="articulo">
                 <img src="${producto.image}" alt="${producto.title}"/>
                 <h6>${producto.title}</h6>
-                <h6>${producto.price}</h6>
                 <div/>`
 
 })
@@ -21,4 +21,24 @@ const pedirDatos = () => {
 }
 
 
+const mostrarProductos = () => {
+    fetch('https://fakestoreapi.com/products', options)
+    .then(res=>res.json())
+    // .then(json=>console.log(json))
+    .then(res => {
+        res.map((producto) => {
+            productos.innerHTML += 
+                `<div class="articulo">
+                <img src="${producto.image}" alt="${producto.title}"/>
+                <h6>${producto.title}</h6>
+                <h6>Precio: ${producto.price}</h6>
+                <button>Añadir al Carrito</button>
+                <div/>`
+
+})
+})
+.catch(error => console.error(error));
+}
+
 pedirDatos()
+mostrarProductos()
